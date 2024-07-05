e119ac6997310300bf1813976a192e3f32dd4e17
Yash shinde <yashshinde.tech@gmail.com>
2024-07-05 20:34:50 +0530

    services provided

---
+++ StudentService.java
@@ -0,0 +1,33 @@
+package com.exmaple.Student.Service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.Student.Repository.*;
+import com.example.Student.Controller.*;
+import com.example.Student.Domain.*;
+@Service
+
+public class StudentService {
+
+	@Autowired
+	private StudentRepository repo;
+	
+	public List <Student> listAll(){
+		return repo.findAll()
+	}
+	
+	public void save (Student std ) {
+		repo.save(std);
+	}
+	
+	public void delete(long id ) {
+		repo.deleteByID(id);
+	}
+	
+	public Student get (long id ) {
+		return repo.findByID(id).get();
+	}
+	 
+
+}
