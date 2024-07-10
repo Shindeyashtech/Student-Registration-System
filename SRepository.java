8921ff6c0836f22f948de2dd3ca1a67860831a22
Yash shinde <yashshinde.tech@gmail.com>
2024-07-10 20:59:07 +0530

    repo

---
+++ SRepository.java
@@ -0,0 +1,11 @@
+package com.example.StudentRegistration.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.StudentRegistration.Domain.Student;
+@Repository
+public interface SRepository extends JpaRepository<Student, Integer> {
+
+	 
+}
