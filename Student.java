b686f44600fb752a470343f1949fb375e0cb07e4
Yash shinde <yashshinde.tech@gmail.com>
2024-07-10 20:58:44 +0530

    domain

--- Student.java
+++ Student.java
@@ -1,5 +1,6 @@
-package com.example.Student.Domain;
+package com.example.StudentRegistration.Domain;
 
+ 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
