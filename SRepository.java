d0b85883bc54419df019e56ff4b1d1d49de1727d
Yash shinde <yashshinde.tech@gmail.com>
2024-08-06 23:28:35 +0530

    Update SRepository.java

--- SRepository.java
+++ SRepository.java
@@ -1,4 +1,4 @@
-package com.example.StudentRegistration.Repository;
+  package com.example.StudentRegistration.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
